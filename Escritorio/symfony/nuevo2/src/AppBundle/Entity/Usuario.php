7e7889407917b07a3c0fb0366077b033427b1828
berto <albertomartinnot@gmail.com>
2017-03-30 09:21:52 +0200

    User Entity

---
+++ Escritorio/symfony/nuevo2/src/AppBundle/Entity/Usuario.php
@@ -0,0 +1,130 @@
+<?php
+
+use Doctrine\ORM\Mapping as ORM;
+
+namespace AppBundle\Entity;
+
+class Usuario{
+
+	private $nombre;
+	private $pass;
+	private $mail;
+    private $edad;
+
+
+
+
+    /**
+     * @var integer
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     *
+     * @return Usuario
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set pass
+     *
+     * @param string $pass
+     *
+     * @return Usuario
+     */
+    public function setPass($pass)
+    {
+        $this->pass = $pass;
+
+        return $this;
+    }
+
+    /**
+     * Get pass
+     *
+     * @return string
+     */
+    public function getPass()
+    {
+        return $this->pass;
+    }
+
+    /**
+     * Set mail
+     *
+     * @param string $mail
+     *
+     * @return Usuario
+     */
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    /**
+     * Get mail
+     *
+     * @return string
+     */
+    public function getMail()
+    {
+        return $this->mail;
+    }
+   
+   /**
+     * Set edad
+     *
+     * @param integer $edad
+     *
+     * @return Usuario
+     */
+    public function setEdad($edad)
+    {
+        $this->edad = $edad;
+
+        return $this;
+    }
+
+    /**
+     * Get edad
+     *
+     * @return integer
+     */
+    public function getEdad()
+    {
+        return $this->edad;
+    }
+   
+
+}
